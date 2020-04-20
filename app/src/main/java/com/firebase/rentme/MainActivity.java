10096e2fe345d5102db32ffc4848041baf15d295
Fishayyy <Mr.ATFisher@gmail.com>
2020-04-20 11:44:52 -0700

    Debugging...

--- app/src/main/java/com/firebase/rentme/MainActivity.java
+++ app/src/main/java/com/firebase/rentme/MainActivity.java
@@ -1,12 +1,18 @@
 package com.firebase.rentme;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.content.Intent;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -16,6 +22,10 @@ import com.firebase.rentme.models.CardViewAdapter;
 import com.firebase.rentme.models.Property;
 import com.firebase.rentme.models.ResultsFilter;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
@@ -39,11 +49,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-import github.nisrulz.easydeviceinfo.base.EasyLocationMod;
-
 public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
 
     private FirebaseFirestore database;
     private CollectionReference propertiesCollection;
@@ -79,7 +88,7 @@ public class MainActivity extends AppCompatActivity
 
         initFilter();
 
-        initToggleButton();
+        initLocationButton();
     }
 
     private void initFirestore()
@@ -318,25 +327,34 @@ public class MainActivity extends AppCompatActivity
         startActivityForResult(intent, 1);
     }
 
-    public void initToggleButton()
+    public void initLocationButton()
     {
         getCurrentLocationButton = findViewById(R.id.currentLocationButton);
         isPressed = false;
-        getCurrentLocationButton.setOnClickListener(new View.OnClickListener() {
+        getCurrentLocationButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v)
             {
-                if(isPressed)
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
                 {
-                    getCurrentLocationButton.setBackgroundResource(R.drawable.location_unpressed);
-                    isPressed = false;
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_LOCATION_PERMISSION);
                 }
                 else
                 {
-                    getCurrentLocationButton.setBackgroundResource(R.drawable.location_pressed);
-                    autocompleteFragment.setText("");
-                    findCurrentLocation();
-                    isPressed = true;
+                    if (isPressed)
+                    {
+                        getCurrentLocationButton.setBackgroundResource(R.drawable.location_unpressed);
+                        isPressed = false;
+                    }
+                    else
+                    {
+                        getCurrentLocationButton.setBackgroundResource(R.drawable.location_pressed);
+                        autocompleteFragment.setText("");
+                        findCurrentLocation();
+                        isPressed = true;
+                    }
                 }
             }
         });
@@ -345,28 +363,52 @@ public class MainActivity extends AppCompatActivity
 
     public void findCurrentLocation()
     {
-        EasyLocationMod easyLocationMod = new EasyLocationMod(getApplicationContext());
-        double[] coordinates = easyLocationMod.getLatLong();
+        final LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        try
-        {
-            List<Address> addresses = geocoder.getFromLocation(coordinates[0], coordinates[1], 1);
-            if (addresses.size() > 0)
-            {
-                Address currentLocation = addresses.get(0);
-                locationQuery = currentLocation.getPostalCode();
-                initRealTimeListener();
-            }
-            else
-            {
-                Toast.makeText(this, "Location Not Found", Toast.LENGTH_SHORT).show();
-            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                .requestLocationUpdates(locationRequest, new LocationCallback()
+                {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult)
+                    {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                .removeLocationUpdates(this);
+                        if (locationResult != null && locationResult.getLocations().size() > 0)
+                        {
+                            int latestLocationIndex = locationResult.getLocations().size() - 1;
+                            double latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                            double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+
+                            try
+                            {
+                                Geocoder geocoder;
+                                List<Address> addresses;
+                                geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+                                if(addresses.size() > 0)
+                                {
+                                    Address currentLocation = addresses.get(0);
+                                    locationQuery = currentLocation.getPostalCode();
+                                    initRealTimeListener();
+                                }
+                                else
+                                {
+                                    Toast.makeText(MainActivity.this, "Location not Found", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            catch (IOException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+                }, Looper.getMainLooper());
     }
 
     @Override
@@ -382,6 +424,20 @@ public class MainActivity extends AppCompatActivity
                 propertyCardList.addAll(propertiesFilter.getFilteredResults(unfilteredProperties));
                 cardAdapter.notifyDataSetChanged();
             }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0)
+        {
+            findCurrentLocation();
+        }
+        else
+        {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
         }
     }
 }
