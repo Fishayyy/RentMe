c5882b50f7cd45029134d3642a18307b6ca675b9
Fishayyy <Mr.ATFisher@gmail.com>
2020-05-20 12:44:15 -0700

    No more bugs

--- app/src/main/java/com/firebase/rentme/account/CreatePropertyActivity.java
+++ app/src/main/java/com/firebase/rentme/account/CreatePropertyActivity.java
@@ -453,6 +453,7 @@ public class CreatePropertyActivity extends AppCompatActivity implements SelectB
     //upload multiple images.
     private void uploadImage()
     {
+        image = ImageList.size() - 1;
         if (upload_count < ImageList.size())
         {
             storageReference.child(System.currentTimeMillis() + "." + getExtension(ImageList.get(image)))
@@ -465,7 +466,7 @@ public class CreatePropertyActivity extends AppCompatActivity implements SelectB
                             retrieveImageURL(taskSnapshot.getStorage().getDownloadUrl());
                             photoURLCounter = ImageList.size();
                             upload_count++;
-                            image++;
+                            image--;
                             uploadImage();
                         }
                     })
@@ -558,7 +559,7 @@ public class CreatePropertyActivity extends AppCompatActivity implements SelectB
                     newProperty.setTimeOfCreation(System.currentTimeMillis());
                     newProperty.setHousingCategory(categorySpinner.getSelectedItem().toString());
                     newProperty.setPrice(Double.parseDouble(editTextPrice.getText().toString()));
-                    newProperty.setPhotoURL(photoURLList.get(photoURLList.size()-1));
+                    newProperty.setPhotoURL(photoURLList.get(0));
                     newProperty.setPhotoURLList(photoURLList);
                     newProperty.setBio(editTextBio.getText().toString());
                     newProperty.setAddress(editTextAddress.getText().toString());
